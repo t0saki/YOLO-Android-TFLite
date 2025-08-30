c9276a77a4c5f81d6ddc5d7203ab42751400bf75
tosaki <t0saki@outlook.com>
2025-08-30 15:55:31 +0800

    first

--- YOLOv10-Object-Detector-Android-Tflite/app/src/main/java/com/surendramaran/yolov10objectdetector/Constants.kt
+++ YOLOv10-Object-Detector-Android-Tflite/app/src/main/java/com/surendramaran/yolov10objectdetector/Constants.kt
@@ -1,6 +1,7 @@
 package com.surendramaran.yolov10objectdetector
 
 object Constants {
-    const val MODEL_PATH = "yolov10n_int8.tflite"
+    const val MODEL_PATH = "yolo11n_int8.tflite"
+    const val OLD_MODEL_PATH_FOR_METADATA = "yolov10n_int8.tflite"
     val LABELS_PATH: String? = null // provide your labels.txt file if the metadata not present in the model
 }
